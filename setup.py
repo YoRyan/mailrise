fd247ea489969f97b9f0f4d0950ac6864ab79393
Ryan Young <ryan@youngryan.com>
2022-05-11 20:35:33 +0000

    chore: update to pyscaffold 4.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for mailrise.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0.2.
+    This file was generated with PyScaffold 4.2.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
--- src/mailrise/__init__.py
+++ src/mailrise/__init__.py
@@ -1,13 +1,15 @@
-"""
-Mailrise is an SMTP gateway for Apprise notifications.
-"""
+import sys
 
-from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
-    DIST_NAME = __name__
-    __version__ = version(DIST_NAME)
+    dist_name = __name__
+    __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
